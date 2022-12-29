dcc177f4607dafae1a2b2d4992bc5309bdd807e0
창준 이 <naromine@naver.com>
2022-12-29 17:48:04 +0900

    (2022.12-29) 자료형 변환

--- Solution/Source.c
+++ Solution/Source.c
@@ -3,100 +3,63 @@
 
 void main()
 {
-	/* 2022-12-28
+	/* 2022-12-29
 
-		조건문이란?
-		어떤 조건이 주어질 때 해당 조건에 따라 동작을 수행하도록 실행하는 명령문
+		형 변환이란?
+		서로 다른 자료형을 가지고 있는 변수끼리 연산이 이루어질 때 기존에 저장했던 자료형을 다른 자료형으로 변환하는 과정
 
-		printf("10은 0보다 크다. 관계 연산자의 값 : %d\n", 10 > 0);
-		printf("10은 0보다 작다. 관계 연산자의 값 : %d\n", 10 < 0);
+		sizeof() 연산자란?
+		피연산자의 크기를 바이트 단위로 변환하는 연산자
 
-		printf("10은 0보다 크거나 같다. 관계 연산자의 값 : %d\n", 10 >= 0);
-		printf("10은 0보다 작거나 같다. 관계 연산자의 값 : %d\n", 10 <= 0);
+		printf("char 자료형의 크기 : %d\n", sizeof(char));
+		printf("short 자료형의 크기 : %d\n", sizeof(short));
+		printf("int 자료형의 크기 : %d\n", sizeof(int));
+		printf("long 자료형의 크기 : %d\n", sizeof(long));
+		printf("float 자료형의 크기 : %d\n", sizeof(float));
+		printf("double 자료형의 크기 : %d\n", sizeof(double));
+		printf("long double 자료형의 크기 : %d\n", sizeof(long double));
 
-		printf("10은 10과 같다. 관계 연산자의 값 : %d\n", 10 == 10);
-		printf("10은 10과 같지 않다. 관계 연산자의 값 : %d\n", 10 != 10);
-
-		관계 연산자는 조건이 맞을 떄 1이라는 값을 반환하며, 조건이 틀릴 때 0이라는 값으로 반환한다.
-
-		if문이란?
-		어떤 특정한 조건을 비교하여 조건이 맞다면 실행되는 명령문
-
-		int health = 100;
-
-		if(health > 0) {
-			printf("Player는 살아있는 상태입니다.");
-		}
-
-		else if문이란?
-		if문의 조건이 틀릴 때 else if문의 조건이 맞다면 실행되는 명령문
-		if문에 연결된 모든 조건문의 조건이 맞을 때 가장 위에 있는 조건문만 실행된다.
-
-		int money = 100;
-
-		if (money > 500) {
-			printf("아이템 구매");
-		}
-		else if (money > 50) {
-			printf("아이템 정보 표시");
-		}
-		else if (money == 100) {
-			printf("아이템 대여");
-		}
-
-		else문이란?
-		if문과 else if문의 조건이 다 틀리면 실행되는 명령문
-
-		int stage = 1;
-
-		if (stage == 1) {
-			printf("첫 번째 스테이지입니다.");
-		}
-		else if (stage == 2) {
-			printf("두 번째 스테이지입니다.");
-		}
-		else {
-			printf("비밀의 방에 도착하였습니다.");
-		}
-
-		카멜 표기법
-		각 단어의 첫 문자를 대문자로 표기하되, 이름의 첫 문자는 소문자로 작성하는 표기법
-		int camelCase = 10;
-
-		파스칼 표기법
-		각 단어의 첫 문자를 대문자로 표기하는 표기법
-		int CountDown, PascalCase = 50;
-
-		스네이크 표기법
-		각 단어의 사이를 '_'로 구분해주는 표기법
-		int Snake_Case = 100;
-
-		헝가리안 표기법
-		이름 앞에 변수의 타입을 접두어로 넣어주는 표기법
-		ch(char), i(int), db(double)
-		int idata = 1000;
-
-		시프트 연산자
-		비트값을 왼쪽 또는 오른쪽으로 이동시키는 연산자
+		암묵적 형 변환이란?
+		서로 다른 자료형으로 연산이 이루어질 떄 자료형의 크기가 큰 자료형으로 변환되는 과정
 
 
+		// 대입 연산 과정 시 발생하는 암묵적 형 변환
+		int integer = 10;
+		float result = integer;
 
+		printf("result 변수의 값 : %f\n", result);
 
+		float data = 3.678;
+		int memory = data;
 
+		// 표현범위가 작은 데이터에 표현범위가 큰 데이터를 저장하게 되면 암묵적으로 데이터 손실이 발생한다.
+		printf("memory 변수의 값 : %d\n", memory);
+
+		정수의 승격
+		char box1 = 10;
+		short box2 = 20;
+
+		printf("box1과 box2를 연산한 결과 : %d\n", box1 + box2);
+		printf("box1과 box2를 연산한 크기 : %d\n", sizeof(box1 + box2));
+
+		피연산자의 자료형 불일치로 발생하는 자동 형 변환
+		float number = 2.93f + 14;
+
+		printf("number의 변수의 값 : %f", number);
+
+		명시적 형 변환이란?
+		연산이 이루어지기 전에 사용자가 직접 자료형을 변환하는 과정
 
 
 	-------------------------------*/
 	
-	int value = 10; // 0000 1010
+	int health = 5;
+	int damage = 2;
 
-	// value << 2 -> 0010 1000
-	printf("value 변수를 왼쪽 2칸 시프트 연산한 결과 : %d\n", value <<= 2);
-	// 0010 1000
-	printf("value 변수의 값 : %d\n", value);
-	// 0001 0100 = 20
-	printf("value 변수를 오른쪽 1칸 시프트 연산한 결과 : %d\n", value >> 1);
+	float result = (float)health / damage;
 
-	printf("value 변수의 값 : %d\n", value);
-
+	// 정수형 변수끼리 연산을 수행하게 되면 정수의 결과값만 가질 수 있다.
+	printf("result 변수의 값 = %f", result);
 	
+
 }
